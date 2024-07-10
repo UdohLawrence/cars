93db72930c4c868b9f757f6f64315bcf5eb5fa48
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-07-10 18:15:09 +0100

    fixed overlay image in the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,14 +24,14 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image 
-          src="/hero.png"
-          alt="Hero Image"
-          fill
-          className="object-contain"
-        />
-          <div className="hero__image-overlay" />
+          <Image
+            src="/hero.png"
+            alt="Hero Image"
+            fill
+            className="object-contain"
+          />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
